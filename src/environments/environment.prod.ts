1d30202a0c6463490ce6aeba2c0eb16c8f2a2514
Tunuguntla Mohana Sai <mohana.sai1204@gmail.com>
2026-05-16 15:37:05 +0530

    [Tunuguntla Mohana Sai] fixed production url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://flowboard-mohana.duckdns.org/'
+  apiUrl: 'https://flowboard-mohana.duckdns.org'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://flowboard-mohana.duckdns.org/'
+  apiUrl: 'https://flowboard-mohana.duckdns.org'
 };
